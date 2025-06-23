8bbf91063b58d3dfb47901b5ec69eb17440bf712
reagan luyinda <luyindareagan@gmail.com>
2025-06-23 15:05:14 +0300

    Feat; Created the main page
    this is the main App page

---
+++ solelabs/src/app/page.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="flex items-center justify-center">
+      <h1 className="text-amber-400 text-4xl ">Welcome Home!</h1>
+    </div>
+  );
+}
+
+export default Home;

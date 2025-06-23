7f196e393ec72d08f5ec5a4c31e484c2c375b461
reagan luyinda <luyindareagan@gmail.com>
2025-06-23 15:06:26 +0300

    Feat; Created routes for About and Contact
    the About and contact routes were created

---
+++ solelabs/src/app/about/page.tsx
@@ -0,0 +1,9 @@
+function About() {
+  return (
+    <div className="text-blue-500 flex items-center justify-center text-3xl">
+      This is the About page
+    </div>
+  );
+}
+
+export default About;
---
+++ solelabs/src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+function Contact() {
+  return (
+    <div className="text-emerald-500 flex items-center justify-center text-3xl">
+      This is the Contact page
+    </div>
+  );
+}
+
+export default Contact;

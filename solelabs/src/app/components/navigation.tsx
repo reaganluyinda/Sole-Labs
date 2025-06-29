628bc546943c0b3486677a4ab18bc4ee05908b35
reagan luyinda <luyindareagan@gmail.com>
2025-06-29 20:55:50 +0300

    ref; change the margin size and font size of the links

--- solelabs/src/app/components/navigation.tsx
+++ solelabs/src/app/components/navigation.tsx
@@ -3,10 +3,10 @@ import React from "react";
 
 function Navigation() {
   return (
-    <nav className="border-2 py-8">
+    <nav className="border-2 py-8 mx-4 md:mx-12 lg:mx-32 my-4">
       <div className="flex justify-between">
         <h1>LOGO</h1>
-        <div className="space-x-8">
+        <div className="space-x-8 font-semibold">
           <Link href="/">Home</Link>
           <Link href="/about">About Us</Link>
           <Link href="/contact">Contact</Link>

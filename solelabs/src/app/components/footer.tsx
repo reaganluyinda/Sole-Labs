688e74cf068edb81d05c3ca539cdf134b05187a8
reagan luyinda <luyindareagan@gmail.com>
2025-06-29 20:56:44 +0300

    ref: Added padding to the footer

--- solelabs/src/app/components/footer.tsx
+++ solelabs/src/app/components/footer.tsx
@@ -1,12 +1,13 @@
 function Footer() {
   return (
-    <footer>
-      <div className="flex justify-between">
+    <footer className="pt-28 pb-8 bg-[#212529] text-[#fff]">
+      <hr className="mb-4 max-w-6xl md:mx-auto mx-4" />
+      <div className="flex flex-col md:flex-row items-center gap-4 md:justify-around">
         <p>
           Copyright &copy; {new Date().getFullYear()} Sole Labs. All rights
           reserved.
         </p>
-        <div className="flex space-x-2">
+        <div className="flex gap-2">
           <p>Terms & Conditions</p>
           <p>|</p>
           <p>Privacy Policy</p>

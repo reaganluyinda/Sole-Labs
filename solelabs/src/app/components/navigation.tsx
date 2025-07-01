9e67afebb5a3275a95a723b442e4c02196872393
reagan luyinda <luyindareagan@gmail.com>
2025-07-01 21:58:02 +0300

    Feat; Updated the nav component
    added hover and selected functionality on the route links and also added the logo image

--- solelabs/src/app/components/navigation.tsx
+++ solelabs/src/app/components/navigation.tsx
@@ -1,15 +1,42 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
+import Image from "next/image";
 
 function Navigation() {
+  const pathname = usePathname();
+
+  const navLinks = [
+    { href: "/", label: "Home" },
+    { href: "/about", label: "About Us" },
+    { href: "/contact", label: "Contact" },
+  ];
   return (
-    <nav className="border-2 py-8 mx-4 md:mx-12 lg:mx-32 my-4">
-      <div className="flex justify-between">
-        <h1>LOGO</h1>
+    <nav className=" mx-4 md:mx-12 lg:mx-32 my-4">
+      <div
+        className="flex justify-between items-center
+      "
+      >
+        <Image
+          src="/logo.png"
+          alt="Logo"
+          width={160}
+          height={160}
+          className="object-contain h-32 w-auto cursor-pointer"
+        />
         <div className="space-x-8 font-semibold">
-          <Link href="/">Home</Link>
-          <Link href="/about">About Us</Link>
-          <Link href="/contact">Contact</Link>
+          {navLinks.map((link) => (
+            <Link
+              key={link.href}
+              href={link.href}
+              className={`hover:text-[#33B3A9] transition-colors ${
+                pathname === link.href ? "text-[#01796f]" : "text-gray-700"
+              }`}
+            >
+              {link.label}
+            </Link>
+          ))}
         </div>
       </div>
     </nav>
